411aea36b6dcac09ce6919c3fa6481fb4374339e
Agustin <agustinbrage19@gmail.com>
2024-09-24 17:08:05 -0300

    feat: stop tracking projects folders, remove all of its tracked content

--- aws/projects/hello-ec2/envs/dev/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "ec2_server_public_ip" {
-  description = "Public IP adress of the EC2 Instance"
-  value       = module.ec2.public_ip
-}
-
-output "ec2_server_private_ip" {
-  description = "Private IP adress of the EC2 Instance"
-  value       = module.ec2.private_ip
-}
